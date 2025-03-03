aaa5c68358168924311831ac2c016af02b45e3c1
Arvtia <116747258+arvtia@users.noreply.github.com>
2025-03-03 18:23:33 +0530

    Update wellness_progms.php

--- himalaya_yoga/wellness_progms.php
+++ himalaya_yoga/wellness_progms.php
@@ -5,11 +5,11 @@
     <!-- Hero Section -->
     <div class="position-relative text-center text-white">
         <!-- Full-Width Image as Background -->
-        <img src="assests/images/ca-4.jpg" class="img-fluid w-100 vh-100 object-fit-cover" alt="ca-4">
+        <img src="assests/images/ca-4-ki-behen.jpg" class="img-fluid  object-fit-cover min-vh-100 w-100" alt="ca-4">
 
         <!-- Centered Text Overlay -->
-        <div class="position-absolute top-50 start-50 translate-middle w-75">
-            <h1 class="fw-bold">Programmes/Campaigns For Overcoming Chronic Lifestyle Diseases</h1>
+        <div class="position-absolute top-50 start-50 translate-middle w-75" data-aos="fade-up" data-aos-duration="2000">
+            <h1 class="fw-bold">Programmes / Campaigns For Overcoming Chronic Lifestyle Diseases</h1>
         </div>
     </div>
 
@@ -18,15 +18,15 @@
 
     </div>
 
-    <div class="text-center pb-4">
-        <h4>Programmes/Campaigns For Overcoming Chronic Lifestyle Diseases</h4>
+    <div class="text-center pb-4 w-75 mx-auto" data-aos="fade-up" data-aos-duration="3000">
+        <h4>Programmes / Campaigns For Overcoming Chronic Lifestyle Diseases</h4>
 
         <p class="fw-bold">Waist Circumference</p>
         <p class="fw-bold">80 cm / 31.5 inches - for females</p>
         <p class="fw-bold">90 cm / 35.5 inches - for males.</p>
     </div>
 
-    <div class="container-fluid p-0">
+    <div class="container-fluid p-0" data-aos="fade-up" data-aos-duration="3000">
         <ol class="list-group list-group-numbered w-75 w-md-100 w-lg-75 w-sm-100 mx-auto p-3 p-md-5">
             <li class="list-group-item bg-success text-white border-0">Belly Fat, Obesity, Overweight</li>
             <li class="list-group-item bg-success text-white border-0">Belly Fat, Fatty Liver</li>
@@ -44,13 +44,13 @@
         </ol>
     </div>
 
-    <div class="text-center">
-        <h3 class="text-success">If you can manage the first three; others are automatically managed…</h3>
-        <p class="text-success">#PRATYAHARA & DHARANA PRACTICES including Yoga Nidra for Relaxing & Calming The Mind From Stresses & Tensions</p>
+    <div class="text-center ">
+        <h4 class="text-success w-75 text-center mx-auto ">If you can manage the first three; others are automatically managed.</h4>
+        <p class="text-success w-75 text-center mx-auto">#PRATYAHARA & DHARANA PRACTICES including Yoga Nidra for Relaxing & Calming The Mind From Stresses & Tensions</p>
         <hr class="solid w-100 ">
     </div>
 
-    <div class="container-fluid d-flex flex-column justify-content-center align-items-center vh-100">
+    <div class="container-fluid d-flex flex-column justify-content-center align-items-center vh-100" data-aos="fade-up" data-aos-duration="3000">
         <div class="col-md-4 text-center">
             <p class="fw-bold text-dark fs-4">Contact for more details</p>
             <img src="assests/images/prabhakar.jpeg" class="w-50 rounded-circle" alt="prabhakar">
