78abdcfffed94dac41d8caea46e255a4ee7e3f99
Arvtia <116747258+arvtia@users.noreply.github.com>
2025-03-08 16:41:52 +0530

    Create index.php
    index.php

---
+++ himalaya_yoga/index.php
@@ -0,0 +1,280 @@
+
+<?php include "header.php" ?>  
+
+
+        
+<div id="carouselExampleInterval" class="carousel slide" data-bs-ride="carousel" data-aos="fade-up" style="overflow: hidden;">
+  <div class="carousel-inner">
+    <div class="carousel-item" data-bs-interval="5000">
+      <img src="assests/images/ca-4.jpg" class="img-fluid w-100 object-fit-cover" alt="">
+      <div class="carousel-caption d-flex flex-column h-100 align-items-center justify-content-center">
+        <h3>PRATYAHARA & DHARANA PRACTICES</h3>
+        <p>for Relaxing & Calming The Mind From Stresses & Tensions</p>
+        <a href="search_result.php" class="btn btn-outline-light px-4 py-2 rounded-0">Learn More</a>
+      </div>
+    </div>
+    <div class="carousel-item" data-bs-interval="2000">
+      <img src="assests/images/get-in-touch.jpg" class="img-fluid w-100" alt="">
+      <div class="carousel-caption d-flex flex-column h-100 align-items-center justify-content-center">
+        <h3>NATURE THERAPY</h3>
+        <p>Nature therapy, also called ecotherapy, is the practice of being in nature to boost growth and healing, especially mental health.</p>
+        <a href="natur_therapy.php" class="btn btn-outline-light px-4 py-2 rounded-0">Learn More</a>
+      </div>
+    </div>
+    <div class="carousel-item active">
+      <img src="assests/images/ca-5.png" class="img-fluid w-100" alt="">
+      <div class="carousel-caption d-flex flex-column h-100 align-items-center justify-content-center">
+        <h3>SPIRITUAL YOGA</h3>
+        <p>It is very essential to find teachings from our ancient wisdom and make them relevant to our present-day problems.</p>
+        <a href="search_result.php" class="btn btn-outline-light px-4 py-2 rounded-0">Learn More</a>
+      </div>
+    </div>
+  </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+</div>
+
+<!--carousel end--->
+
+<div class="container-fluid mx-auto my-auto py-2 bg-success" data-aos="zoom-out-right">
+  <p class="text-center my-auto text-white"><strong>New: </strong> PRATYAHARA & DHARANA PRACTICES for Relaxing & Calming The Mind From Stresses & Tensions</p>
+</div>
+
+  <div class="container-fluid py-3 bg-white mx-auto">
+    <div class="col-lg-11 col-md-11 col-sm-12 mx-auto">
+            <div class="row flex align-items-center py-5">
+                <div class="col-lg-6 col-sm-12 col-md-6 mx-auto" data-aos="zoom-in-right">
+                  <img src="assests/images/photo-1.jpg" alt="photo-1" class="d-block mx-auto img-fluid" width="500" height="500" loading="lazy" data-aos="fade-up" data-aos-anchor-placement="top-center">
+            </div>
+          <div class="col-lg-6 col-md-6 col-sm-12" data-aos="zoom-in-right">
+              <h1 class="display-5 fw-bold text-body-emphasis lh-1 mb-3">About us</h1>
+              <p class="lead w-100">Himalaya Yoga and Holistic Living Institute (HYHLI ) was constituted in 2015 with a view to pursuing the mission of projecting the authentic teachings of yoga and spiritual living to the world. HYHLI is an autonomous body with no other organization and administrative or legal associations.</p>
+              <a href="about-us.php"><button type="button" class="btn btn-warning px-4 rounded-pill">Read more</button></a>
+          </div>
+        </div>
+      </div>`
+    </div>
+<!--seond hereos--> 
+
+  <div class="container-fluid  py-3 bg-white mx-auto">
+    <div class="col-lg-10 col-md-10 col-sm-12 mx-auto">
+        <div class="row flex align-items-center py-4">
+          <div class="col-lg-6 col-sm-12 col-md-6 mx-auto" data-aos="zoom-in-right">
+            <h1 class=" display-5 fw-bold text-body-emphasis lh-1 mb-3">Programmes / Campaigns For Overcoming Chronic Lifestyle Diseases</h1>
+              <a href="wellness_progms.php" class="px-4"><button type="button" class="btn btn-warning px-4 rounded-pill">Read more</button></a>
+          </div>
+          <div class="col-lg-6 col-md-6 col-sm-12 mx-auto pt-2">
+            <img src="assests/images/photo-2.jpg" alt="photo-1" class="d-block mx-auto img-fluid" width="500" height="500" loading="lazy" data-aos="fade-up" data-aos-anchor-placement="top-center" >
+          </div>
+        </div>
+      </div>
+  </div>
+
+<!--responsive video-->
+
+  <div class="embed-responsive embed-responsive-16by9 w-100 pt-5 mb-0 position-relative" data-aos="fade-up" data-aos-duration="3000">
+    <video src="assests/images/vide01.mp4" class="embed-responsive-item w-100 object-fit-cover min-vh-100" autoplay muted loop>
+    </video>
+    <div class="text-start px-4 position-absolute top-50" data-aos="fade-up" data-aos-anchor-placement="bottom-center">
+      <h1 class="text-white">YOGA</h1>
+      <p class="lead text-warning fs-5">“True yoga is not about the shape of your body, but the shape of your life.”</p>
+      <a href="courses.php"><button class="bg-dark text-warning py-2 px-3 rounded">Register Now</button></a>
+    </div>
+  </div>
+<div class="container-fluid d-flex justify-content-center bg-warning py-4" data-aos="fade-up-right">
+  <h4 class="w-80 text-white text-center py-4 fs-3">“Today, Scientific research have established that the spiritual practices that our ancients practiced are immensely helpful in dealing with these stressful conditions."</h4>
+</div>
+
+
+<!--video with text text--> 
+
+<div class="container-fluid py-3 bg-white mx-auto">
+   <div class="col-lg-10 col-md-10 col-sm-12">
+      <div class="row align-items-center py-5">
+            <div class="col-sm-12 col-lg-6 col-md-6">
+              <video src="assests/images/video03.mp4" alt="video03" class="d-block mx-auto img-fluid" width="300" height="900" autoplay muted loop   id="video-1-1">
+            </div>
+            <div class="col-lg-6 col-md-6 col-sm-12" data-aos="fade-right" data-aos-offset="300" data-aos-easing="ease-in-sine">
+                <h1 class="fw-bold text-body-emphasis lh-1 py-4 ">Meditation Class</h1>
+                <p class="lead text-dark">Our ancient wisdom is of everlasting value and universal applicability, and it is not only for monks and full-time spiritual practitioners but also to the lay public as well.</p>
+                <a href="search_result.php"><button type="button" class="btn btn-warning px-4 rounded-pill">Learn more</button></a>
+            </div>
+        </div>
+    </div>
+  </div>
+
+  <!--second video text -->
+
+
+  <!-- <div class="container-fluid bg-white">
+      <div class="col-lg-10 col-md-10 col-sm-12">
+          <div class="row align-items-center py-5">
+            <div class="col-md-8 col-sm-12 col-lg-8">
+              <video src="assests/images/video04.mp4" alt="video04" class="d-block" width="900" height="300"  autoplay muted loop></video>
+            </div>
+            <div class="col-lg-4 col-sm-12 col-md-4" data-aos="zoom-out-left">
+              <h1 class="display-5 fw-bold text-body-emphasis lh-1 mb-3">Power Yoga</h1>
+                <p class="lead">Today, Scientific research has established that our ancients’ spiritual practices are beneficial in dealing with these stressful conditions.</p>
+                <a href="search_result.php"><button type="button" class="btn btn-warning px-4 rounded-pill">Learn more</button></a>
+            </div>
+          </div>
+      </div>
+  </div> -->
+
+
+<!-- videos with text and button on top-->
+
+  <div class="w-100  position-relative  object-fit-cover">
+    <video src="assests/images/video02.mp4" class="embed-responsive-item w-100 object-fit-cover min-vh-100" autoplay muted loop  data-aos="fade-right" data-aos-offset="300" data-aos-easing="ease-in-sine">
+    </video>
+
+    <div class="text-center my-auto custom-height position-absolute px-5 top-50 start-25 lg-lt-4">
+      <h1 class="text-start align-items-center text-white lh-lg fw-bolder">Meditation</h1>
+      <p class="text-white fs-5">Brilliant things happens in calm mind</p>
+        <a href="search_result.php"><button type="button" class="btn btn-warning px-4 rounded-pill">Learn more</button></a>
+    </div>
+  </div>
+
+  <!--cross boxes---->
+
+
+<!--colum and row boxes-with image and text - icons-->
+
+
+
+
+<div class="container-fluid pt-2">
+  <div class="col-lg-10 col-md-10 col-sm-12 mx-auto pt-2">
+    <div class="row pt-5 position-relative">
+      <h1 class="w-100 text-center fw-bold py-4">Wellness Program</h1>
+      <div class="col-lg-10 col-md-10 col-sm-12 mx-auto">
+        <!-- This container is block on small screens and flex on md+ -->
+        <div class="d-block d-md-flex justify-content-between">
+          <!-- Nature Therapy Column -->
+          <div class="col-12 col-md-6 px-3 mb-3 mb-md-0">
+            <div class="position-relative px-3">
+              <h4 class="text-center bg-success py-4 fs-4 text-white fw-bold">Nature Therapy</h4>
+              <div class="position-relative">
+                <img src="assests/images/photo-box-2-1.jpg" alt="photo-box" class="object-fit-cover img-fluid py-3">
+              </div>
+              <div class="position-absolute top-50 start-50 translate-middle text-center w-100">
+                <p class="text-white fs-6 m-0 w-75">Some types of alternative therapy have been well-researched by scientists, so doctors can safely recommend them to patients. Examples of this include yoga and meditation, as well as acupuncture.</p>
+                <a href="natur_therapy.php"><button type="button" class="btn btn-warning px-4 rounded-pill">Learn more</button></a>
+              </div>
+            </div>
+          </div>
+          <!-- Alternative Therapy Column -->
+          <div class="col-12 col-md-6 px-3">
+            <div class="position-relative px-3">
+              <h4 class="text-center bg-warning py-4 fs-4 text-white fw-bold">Alternative Therapy</h4>
+              <div class="position-relative">
+                <img src="assests/images/photo-box-1-11.jpg" alt="photo-box" class="object-fit-cover img-fluid py-3">
+              </div>
+              <div class="position-absolute top-50 start-50 translate-middle text-center w-100">
+                <p class="text-white fs-6 w-75">Some types of alternative therapy have been well-researched by scientists, so doctors can safely recommend them to patients. Examples include yoga, meditation, and acupuncture.</p>
+                <a href="alternative_therapy.php"><button type="button" class="btn btn-warning px-4 rounded-pill">Learn more</button></a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
+
+
+
+
+<!--form - get in touch with us on page--->
+
+<div class="col-lg-10 col-md-10 col-sm-12 mx-auto py-5">
+<h1 class="display-5 fw-bold text-body-emphasis lh-1 mb-3 text-start">Get in touch with us</h1>
+
+    <div class="row flex-lg-row-reverse align-items-center py-3 w-100 mx-auto">
+      <div class="col-md-6 col-lg-6 col-sm-12 col-lg-6 mx-auto">
+        <img src="assests/images/GETIN.jpg" alt="photo-1" class="d-block mx-auto img-fluid" width="500" height="500" loading="lazy">
+      </div>
+      <div class="col-lg-6 col-md-6 col-sm-12" data-aos="fade-up">
+
+          <form data-aos="fade-up">
+              <div class="form-row d-flex align-items-start">
+                <div class="form-group col p-2">
+                  <label for="inputFirstName">First Name</label>
+                  <input type="text" class="form-control" id="inputFirstName" placeholder="Email">
+                </div>
+                <div class="form-group col p-2">
+                  <label for="inputLastName">Last name</label>
+                  <input type="text" class="form-control" id="inputLastName" placeholder="Last name">
+                </div>
+              </div>
+              <div class="form-group p-2">
+                <label for="inputAddress">Email</label>
+                <input type="text" class="form-control" id="inputAddress" placeholder="example@gmail.com">
+              </div>
+              <div class="form-group p-2">
+                <label for="inputAddress2">Phone number</label>
+                <input type="number" class="form-control" id="phoneNumber" placeholder="">
+              </div>
+              <div class="form-row p-2">
+                <div class="mb-3">
+                    <label for="validationTextarea">Textarea</label>
+                      <textarea class="form-control" id="validationTextarea" placeholder=""></textarea>
+                  <div class="invalid-feedback p-2">
+                            Please enter a message in the textarea.
+                  </div>
+                </div>
+              </div>
+              <button type="submit" class="btn btn-primary px-3 py-3 ">Submit</button>
+          </form>
+      </div>
+    </div>
+</div>
+<!-- testimonials-->
+
+
+<div class="col-lg-10 col-md-10 col-sm-11 mx-auto">
+    <div class="text-center d-flex align-items-center justify-content-center">
+                <h1>Testimonial</h1>
+                <br>
+            </div>
+            
+        <hr class="hr w-100 mx-auto custom-hr-height"/>
+            <div class="row mx-auto my-auto">
+                <div class="col-md-4  d-flex flex-column justify-content-center px-4 my-auto bg-primary">
+                    <img src="assests/images/testimonials-1.png" class="rounded-circle mx-auto" height="100" alt="testimoni1">
+                    <p class="fw-bold fs-6 w-100 text-center">Jason Stobbard</p>
+                    <p class="w-75 text-center mx-auto">“…Absolutely fantastic work, many thanks for the perfect collaboration so far, very much appreciated!…”</p>
+                </div>
+                <div class="col-md-4  d-flex flex-column justify-content-center px-4 my-auto">
+                    <img src="assests/images/testimonials-2.png" class="custom-height-test mx-auto" height="100" alt="testimoni2">
+                    <p class="fw-bold fs-6 w-100 text-center">Jane Austin</p>
+                    <p class="w-75 text-center mx-auto">“… I love this team! They did fantastic work, many thanks for the perfect collaboration so far, very much appreciated!…”</p>
+                </div>
+                <div class="col-md-4  d-flex flex-column justify-content-center px-4 my-auto">
+                    <img src="assests/images/testimonials-3.png" class="custom-height-test mx-auto" height="100" alt="testimoni1">
+                    <p class="fw-bold fs-6 w-100 text-center">Jason Stobbard</p>
+                    <p class="w-75 text-center mx-auto">“…Absolutely fantastic work, many thanks for the perfect collaboration so far, very much appreciated!…”</p>
+                </div>
+            </div>
+  </div>
+  </div>
+
+
+<?php include "footer.php" ?>
+
+
+
+
+
+
+
+
+
+    
